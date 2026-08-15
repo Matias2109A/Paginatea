f5770902f73f2c4a81f1f31a841919782a2ae840
Matias <matiasavalle37@gmail.com>
2026-08-15 20:07:31 -0300

    Solucion a main.jsx

--- PaginaTea/src/main.jsx
+++ PaginaTea/src/main.jsx
@@ -3,27 +3,23 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Nav from './components/Nav'
-import Inicio from './pages/Inicio/Inicio'
-import Introduccion from './pages/Introduccion/Introduccion'
-import Historia from './pages/Historia/Historia'
-import Neurodivergencia from './pages/Neurodivergencia/Neurodivergencia'
+import PaginaPrincipal from './PaginaPrincipal'
 import Blog from './pages/Blog'
 import Eventos from './pages/Eventos'
 import Contactos from './pages/Contactos'
 import Fotter from './components/Footer'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouterStrictMode>
+  <StrictMode>
+  <BrowserRouter>
     <Nav paginaActiva='Inicio' />
     <Routes>
-      <Route path="/" element={<Inicio />} />
+      <Route path="/" element={<PaginaPrincipal />} />
       <Route path="/blog" element={<Blog />} />
       <Route path="/eventos" element={<Eventos />} />
       <Route path="/contactos" element={<Contactos />} />
     </Routes>
-    <Introduccion />
-    <Historia />
-    <Neurodivergencia />
     <Fotter />
-  </BrowserRouterStrictMode>
+  </BrowserRouter>
+  </StrictMode>
 )
